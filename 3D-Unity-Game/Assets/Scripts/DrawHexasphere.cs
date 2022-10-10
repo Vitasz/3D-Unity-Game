7babda6fa7600844d32fccef2d7b487040067023
Vitasz <bogomiavitalii@mail.ru>
2022-10-10 21:07:31 +0300

    add camera rotation

--- 3D-Unity-Game/Assets/Scripts/DrawHexasphere.cs
+++ 3D-Unity-Game/Assets/Scripts/DrawHexasphere.cs
@@ -16,7 +16,7 @@ public class DrawHexasphere : MonoBehaviour
     private MeshFilter _meshFilter;
     private Hexasphere _hexasphere;
     private MeshCollider _meshCollider;
-
+    public CameraSphere cameraSphere;
     public float rotationSpeed = 2f;
     private void Start()
     {
@@ -25,6 +25,23 @@ public class DrawHexasphere : MonoBehaviour
         DrawHexasphereMesh();
     }
 
+    public void OnMouseDown()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            Vector3 position = transform.InverseTransformPoint(hit.point);
+            //Tile tile = _hexasphere.GetTile(position);
+           // tile.SetColor(new Color(Random.value, Random.value, Random.value));
+            //_hexasphere.UpdateMeshDetails();
+            //_mesh.colors = _hexasphere.MeshDetails.Colors.ToArray();
+        }
+    }
+    public void OnMouseDrag()
+    {
+        cameraSphere.RotateAround();
+    }
 
     private void DrawHexasphereMesh()
     {
@@ -43,24 +60,5 @@ public class DrawHexasphere : MonoBehaviour
 
         _meshCollider.convex = true;
     }
-    private void _OnMouseDown()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(inputRay, out hit))
-        {
-            Vector3 position = transform.InverseTransformPoint(hit.point);
-            Tile tile = _hexasphere.GetTile(position);
-            tile.SetColor(new Color(Random.value, Random.value, Random.value));
-            _hexasphere.UpdateMeshDetails();
-            _mesh.colors = _hexasphere.MeshDetails.Colors.ToArray();
-        }
-    }
-    private void OnMouseDrag()
-    {
-        float XaxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
-        float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
-        // select the axis by which you want to rotate the GameObject
-        transform.Rotate(new Vector3(YaxisRotation, -XaxisRotation,0));
-    }
+    
 }
